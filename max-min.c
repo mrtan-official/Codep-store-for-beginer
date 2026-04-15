b11361c244bd15add1130dfd15d15aa65db0f3e2
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2026-04-15 19:38:58 +0600

    Update max-min.c

--- max-min.c
+++ max-min.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main() {
-    int n;
+    int n, arr[n];
     scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
     int max = arr[0];
     int min = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) {
+    for (int i = 1; i < n; i++)
+        {
+        if (arr[i] > max)
+        {
             max = arr[i];
         }
-        if (arr[i] < min) {
+        if (arr[i] < min)
+        {
             min = arr[i];
         }
     }
